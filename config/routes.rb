3e948247a4e61d92d2ee0bff48c5cfc4c9a73cc5
christoandrew <andrew.christoandrew.christo@gmail.com>
2015-11-29 22:32:42 +0300

    Removed precompiled assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  resources :tags
-  resources :articles
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'home#index'
 
   devise_for :admin
