6f15a1f03d8c039521d2b7f8b6486d841342797c
Christo Andrew <andrew.christoandrew.christo@gmail.com>
2016-04-24 13:35:58 +0300

    Added aws config

---
+++ config/initializers/AWS_CONFIG.rb
@@ -0,0 +1,7 @@
+AWS_CONFIG = {
+    'access_key_id' => 'AKIAJNGEXG6MSZCS47VA',
+    'secret_access_key' => '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',
+    'bucket' => 'todidea',
+    'acl' => 'public-read',
+    'key_start' => 'uploads/'
+}
